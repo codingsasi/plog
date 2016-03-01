f3909356d3f86a58f8e153552ad583ebbc1b5d07
Abhai Sasidharan <abhaisasidharan@gmail.com>
2016-03-01 09:43:26 +0530

    NoReverseMatch error

--- api/urls.py
+++ api/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^duration/(?P<duration>[0-9]+)$', views.StoryFilter.as_view(), name='api-filter'),
     url(r'^submit$', views.StorySubmit.as_view(), name='api-submit'),
     #url(r'^api-auth/', include('rest_framework.urls')),
-    url(r'^login/$', views.Login.as_view(), name='api-login'),
-    url(r'^logout/$', views.logout_view, name='logout'),
+    url(r'^login$', views.Login.as_view(), name='api-login'),
+    url(r'^logout$', views.logout_view, name='logout'),
 ]
